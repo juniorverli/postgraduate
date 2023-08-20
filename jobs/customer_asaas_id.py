ab9728831a7d5a4683a1287d9b1ded8b499225d5
Dionei Junior <dionei.junior@outlook.com>
2023-08-20 19:02:24 -0300

    second commit

---
+++ jobs/customer_asaas_id.py
@@ -0,0 +1,74 @@
+from time import sleep
+import requests
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+def registra_cliente_na_api(nome, sobrenome, cpf):
+    url = 'https://sandbox.asaas.com/api/v3/customers'
+
+    payload = {
+        "name": f'{nome} {sobrenome}',
+        "cpfCnpj": f'cpf'
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "access_token": access_token
+    }
+
+    try:
+        sleep(0.5)
+        response = requests.post(url, json=payload, headers=headers)
+        data = response.json()
+
+        if 'erro' in data:
+            return print("CPF Inválido")
+        else:
+            asaas_id = data.get('id', '')
+            return asaas_id
+
+    except requests.RequestException:
+        return registra_cliente_na_api(nome, sobrenome, cpf)
+
+def busca_e_insere_dados(host, database, user, password):
+    try:
+        # Conecta ao banco de dados PostgreSQL
+        with psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password
+        ) as connection:
+
+            # Cria um cursor para executar comandos SQL
+            with connection.cursor() as cursor:
+
+                # Executa a consulta SQL para buscar os dados
+                sql = "SELECT name, surname, cpf FROM customers"
+                cursor.execute(sql)
+
+                # Recupera as linhas e imprime os resultados
+                for row in cursor:
+                    name, surname, cpf = row
+                    print(f"Name: {name}, Surname: {surname}, CPF: {cpf}")
+                    asaas_id = registra_cliente_na_api(name, surname, cpf)
+                    with connection.cursor() as cursor_update:
+                        update = f"UPDATE customers SET customer_asaas_id = '{asaas_id}' WHERE cpf = '{cpf}'"
+                        cursor_update.execute(update)
+
+    except (Exception, psycopg2.Error) as error:
+        print("Erro ao buscar dados:", error)
+
+if __name__ == "__main__":
+
+    load_dotenv()
+    access_token = os.getenv("ACCESS_TOKEN")
+
+    busca_e_insere_dados(
+        host="localhost",
+        database="mydb",
+        user="myuser",
+        password="mysecretpassword",
+
+    )
---
+++ postgresql/init.sql
@@ -0,0 +1,54 @@
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    birthday_date DATE NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    cep VARCHAR(8) NOT NULL,
+    state VARCHAR(2) NULL,
+    city VARCHAR(255) NULL,
+    street VARCHAR(255) NULL,
+    street_number INT NULL,
+    phone_number VARCHAR(11) NOT NULL,
+    card_number VARCHAR(20) NOT NULL,
+    card_month VARCHAR(2) NOT NULL,
+    card_year VARCHAR(4) NOT NULL,
+    card_cvv INT NOT NULL,
+    customer_asaas_id VARCHAR(50) NULL
+);
+
+INSERT INTO customers (
+    name,
+    surname,
+    birthday_date,
+    email,
+    cpf,
+    cep,
+    state,
+    city,
+    street,
+    street_number,
+    phone_number,
+    card_number,
+    card_month,
+    card_year,
+    card_cvv
+)
+VALUES (
+    'Pedro',
+    'Santos',
+    '1995-06-22',
+    'pedro.santos@testxpeducacao.com.br',
+    '67425765101',
+    '48000001',
+    'BA',
+    'Alagoinhas',
+    'Rua Mílton Santos França',
+    '754',
+    '95914336002',
+    '5317727644864293',
+    '10',
+    '2026',
+    '375'
+);
